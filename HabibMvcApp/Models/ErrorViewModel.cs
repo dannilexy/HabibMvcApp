fcd02ecbce7df8e4852e047641d02198d3f0b1a7
El-Dorado <Dannilexy@gmail.com>
2025-09-22 22:35:12 +0100

    Add project files.

---
+++ HabibMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HabibMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
